aa192f86a47b7c602c821369246af1cdbc700441
c-rter <c@rterlawson.com>
2021-04-05 21:52:56 -0400

    resize bg

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,7 +60,7 @@ const App = () => {
     height: "100vh",
     "background-size": "cover",
     "background-image":
-      'linear-gradient(to bottom, rgba(16, 46, 69, 0.90), rgba(0, 0, 0, 1)), url("../../bg1.jpg")',
+      'linear-gradient(to bottom, rgba(16, 46, 69, 0.90), rgba(0, 0, 0, 1)), url("../../bg2.jpg")',
   }
 
   /*
